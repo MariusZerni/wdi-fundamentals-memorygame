6448ede541523a54878009f7fa6520b8aabdca52
Marius Zerni <marius@Amalias-MacBook-Air.local>
2020-01-05 17:59:06 +0000

    "created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,26 @@
+console.log('Up and running');
+
+
+
+
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+  } else {
+	alert("Sorry try again");
+  }
+}
+
+cardsInPlay;
+
+
+console.log('User flipped' + ' ' + cardsInPlay);
