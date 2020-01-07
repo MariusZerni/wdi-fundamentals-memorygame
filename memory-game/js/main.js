41ce57b8ba79576b94ad2ee81f3a0ea878e5abe1
Marius Zerni <marius@Amalias-MacBook-Air.local>
2020-01-07 17:23:20 +0000

    creayed game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,11 +1,29 @@
 console.log('Up and running');
 
 
+let cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 
-
-
-
-let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -16,18 +34,19 @@ function checkForMatch() {
 	}
 }
 
-
 function flipCard(cardId) {
 
-	console.log('User flipped' + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log('User flipped' + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		 checkForMatch();
      }
-
- 	
 }
+
+alert('Sorry, try again');
 
 flipCard(0);
 flipCard(2);
